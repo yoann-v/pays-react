4432fa02c6826d4deeb6739e4d6922eaac83589f
Yoann <yos59540@gmail.com>
2021-10-21 17:04:28 +0200

    tri par radio

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -7,6 +7,8 @@ const Countries = () => {
     const [sortedData, setSortedData] = useState([]);
     const [playOnce, setPlayOnce] = useState(true);
     const [rangeValue, setRangeValue] = useState(40);
+    const [selectedRadio, setSelectedRadio] = useState('');
+    const radios = ["Africa", "America", "Asia", "Europe", "Oceania"];
 
     useEffect(() => {
         if (playOnce) {
@@ -37,9 +39,25 @@ const Countries = () => {
         <div className="countries">
             <div className="sort-container">
                 <input type="range" min="1" max="250" value={rangeValue} onChange={(e) => setRangeValue(e.target.value)} />
+                <ul>
+                    {radios.map((radio) => {
+                        return (
+                            <li key={radio}>
+                                <input type="radio" value={radio} id="{radio}" checked={radio == selectedRadio} onChange={(e) => setSelectedRadio(e.target.value)} />
+                                <label htmlfor={radio}>{radio}</label> 
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
+            <div className="cancel">
+                {selectedRadio && <h5 onClick={() => setSelectedRadio("")}>Annuler recherche</h5>}
             </div>
             <ul className="countries-list">
-                {sortedData.map((country) => (
+                {sortedData
+                    .filter((country) => country.region.includes
+                    (selectedRadio))
+                    .map((country) => (
                     <Card country={country} key={country.name.common} />
                 ))}
             </ul>
